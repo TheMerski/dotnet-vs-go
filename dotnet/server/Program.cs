b4a600c5ccdea1803037f88cf95e4ad55bac7261
Merwin Hill <merwin@q42.nl>
2023-12-05 16:54:18 +0100

    feat(otel): fix otel on .NET server

--- dotnet/server/Program.cs
+++ dotnet/server/Program.cs
@@ -22,10 +22,7 @@ otel.WithTracing(tracing =>
   tracing.AddAspNetCoreInstrumentation();
   tracing.AddHttpClientInstrumentation();
   tracing.AddSource("server.Services.GenericService");
-  tracing.AddOtlpExporter(opt =>
-  {
-    opt.Endpoint = new Uri("localhost:4317");
-  });
+  tracing.AddOtlpExporter();
 });
 
 // Add services to the container.
@@ -37,7 +34,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GenericService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-app.MapPrometheusScrapingEndpoint("localhost:8080/metrics");
+app.MapPrometheusScrapingEndpoint();
 
 // Enable gRPC reflection
 app.MapGrpcReflectionService();
