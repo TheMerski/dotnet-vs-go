21fbd20b48c485c08d2a05508f832672a15aaa6d
Merwin Hill <merwin@q42.nl>
2023-12-06 20:26:17 +0100

    ci: Added script to setup required packages in a cloud shell

---
+++ k6-setup-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Installing just package"
+mkdir -p ~/bin
+# download and extract just to ~/bin/just
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/bin
+# add `~/bin` to the paths that your shell searches for executables
+# this line should be added to your shells initialization file,
+# e.g. `~/.bashrc` or `~/.zshrc`
+export PATH="$PATH:$HOME/bin"
+
+# Install k6
+echo "Installing k6 package"
+sudo gpg -k
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt update
+sudo apt install k6
